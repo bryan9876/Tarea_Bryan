9312c99a45075d570dcbb49acd64ca3f31b4cc30
bryan9876 <91569580+bryan9876@users.noreply.github.com>
2021-11-19 22:12:15 -0600

    Update Parte1.m
    .

--- Parte1.m
+++ Parte1.m
@@ -1,8 +1,8 @@
-%descricion:  para graficar funciones polinomiales 
-%author: Jafet martinez meza 
+%descricion: Graficar funciones polinomiales 
+%date: 202123065
+%author: Bryan Alejandro Martinez López
 %fecha: 19/11/2021
-%date: 202123653
 x=-20:1:20;
 y= (x+2).*(x-2);
 plot (x,y);
-title (" no hay valores para 0, es una funcion polinomial ");
+title (" no existen valores para 0, ya que es una funcion polinomial ");
