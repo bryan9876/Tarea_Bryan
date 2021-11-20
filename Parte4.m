eb8d4adf859f91be66b80bb1e312353e926a08d8
bryan9876 <91569580+bryan9876@users.noreply.github.com>
2021-11-19 22:21:11 -0600

    Update Parte4.m

--- Parte4.m
+++ Parte4.m
@@ -1,8 +1,11 @@
-%descricion:  para graficar funciones polinomiales 
-%author: Jafet martinez meza 
+%descricion: Graficar funciones polinomiales 
+%date: 202123065
+%author: Bryan Alejandro Martinez López
 %fecha: 19/11/2021
-%date: 202123653
+
+
 x=-20:1:20;
 y=((2.*(x.^2))+(x.^4)+x);
+
 plot(x,y);
-title ("valores par cero cuando x=-1, x=0, x=1");
+title ("los valores de cero cuando x=-1, x=0, x=1");
