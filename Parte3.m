d4253e2105bf3515d7dc8e7a62444e397f4c606c
bryan9876 <91569580+bryan9876@users.noreply.github.com>
2021-11-19 22:22:29 -0600

    Update Parte3.m

--- Parte3.m
+++ Parte3.m
@@ -1,4 +1,4 @@
-%descricion:  para graficar funciones polinomiales 
+%descricion: Graficar funciones polinomiales 
 %date: 202123065
 %author: Bryan Alejandro Martinez López
 %fecha: 19/11/2021
@@ -7,4 +7,4 @@ x=-20:1:20;
 x=((x.^2).^1/3);
 
 plot (x,y);
-title ("no existen valores para cero, es una funcion racional ");
+title ("no existen valores para cero, ya que es una funcion racional ");
